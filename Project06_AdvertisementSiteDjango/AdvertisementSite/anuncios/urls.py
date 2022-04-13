91e6de8014756114eead6eb74f9bb031c886a00a
LFHarada <lucasfigueiraharada@protonmail.com>
2022-04-13 02:10:18 -0300

    Commit

--- Project06_AdvertisementSiteDjango/AdvertisementSite/anuncios/urls.py
+++ Project06_AdvertisementSiteDjango/AdvertisementSite/anuncios/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
 
+from .views import homepage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('anuncios.urls'))
+    path('', homepage, name='homepage')
 ]
