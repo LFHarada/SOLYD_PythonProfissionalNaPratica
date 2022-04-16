03b20745a66e5bec6f9037503cc14718800a3181
LFHarada <lucasfigueiraharada@protonmail.com>
2022-04-16 04:36:04 -0300

    commit

--- Project06_AdvertisementSiteDjango/project_website/project_website/secretkey.py
+++
@@ -1,1 +0,0 @@
-SECRETKEY_HIDDEN = 'django-insecure-qv2)uybvz6_t@6j^kv5ro9m!r_b1)1+mi-3h$7-$k!$94^ms5s'
--- Project06_AdvertisementSiteDjango/project_website/project_website/settings_sample.py
+++ Project06_AdvertisementSiteDjango/project_website/project_website/settings_sample.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qv2)uybvz6_t@6j^kv5ro9m!r_b1)1+mi-3h$7-$k!$94^ms5s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
