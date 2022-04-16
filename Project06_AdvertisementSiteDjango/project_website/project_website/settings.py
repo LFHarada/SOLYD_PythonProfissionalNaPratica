92bb489faf4cf46dcce1840c7150e89aa58e841b
LFHarada <lucasfigueiraharada@protonmail.com>
2022-04-16 02:56:42 -0300

    commit

--- Project06_AdvertisementSiteDjango/project_website/project_website/settings.py
+++ Project06_AdvertisementSiteDjango/project_website/project_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+# from secretkey import SECRETKEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
